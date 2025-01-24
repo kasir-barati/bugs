6a0a8a1129d42b935ba6e8c0f683830a066e2d0f
Kasir Barati <kasir.barati@gmail.com>
2025-01-24 20:13:42 +0100

    chore: add test for redis in general

--- test/cart.e2e-spec.ts
+++ test/cart.e2e-spec.ts
@@ -3,7 +3,31 @@ import { RedisContainer } from '@testcontainers/redis';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
+import { createClient } from 'redis';
 import { CartModule } from '../src/cart/cart.module';
+
+describe.only('test redis', () => {
+  it('should work', async () => {
+    const container = await new RedisContainer('redis:alpine')
+      .withExposedPorts({ host: 6379, container: 6379 })
+      // .withUser('username')
+      // .withPassword('password')
+      .start();
+
+    console.log(container.getConnectionUrl());
+
+    const client = createClient({ url: container.getConnectionUrl() });
+
+    await client.connect();
+    expect(client.isOpen).toBeTruthy();
+
+    await client.set('key', 'val');
+    expect(await client.get('key')).toBe('val');
+
+    await client.disconnect();
+    await container.stop();
+  });
+});
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
@@ -12,10 +36,6 @@ describe('AppController (e2e)', () => {
     const container = await new RedisContainer('redis:alpine')
       .withExposedPorts({ host: 6379, container: 6379 })
       .start();
-    console.log(container.getPort());
-    console.log(container.getHost());
-
-    console.log(111111);
 
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [CartModule],
@@ -23,9 +43,13 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
+  }, 22222222222 + 1111111111);
 
   it('/ (GET)', () => {
     return request(app.getHttpServer()).get('/cart').expect(200).expect('Hi');
   });
 });
+
+function sleep() {
+  return new Promise((res, rej) => setTimeout(res, 22222222222));
+}
