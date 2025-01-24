c67e10e7b75e877497ef9117695cbfafaa38a75e
Mohammad Jawad Barati <mohammad.jawad.barati@energy-robotics.com>
2025-01-24 09:00:45 +0100

    chore: wip -- what is Worker requires a connection

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,7 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AudioConsumer } from './audio.consumer';
 
 @Module({
   imports: [
@@ -8,7 +9,7 @@ import { UserService } from './user.service';
       name: 'audio',
     }),
   ],
-  providers: [UserService],
+  providers: [UserService, AudioConsumer],
   exports: [UserService],
 })
 export class UserModule {}
--- test/cart.e2e-spec.ts
+++ test/cart.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { RedisContainer } from '@testcontainers/redis';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
@@ -8,6 +9,11 @@ describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
+    const container = await new RedisContainer('redis:alpine')
+      .withExposedPorts({ host: 6379, container: 6379 })
+      .start();
+    console.log(container.getPort());
+    console.log(container.getHost());
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [CartModule],
     }).compile();
