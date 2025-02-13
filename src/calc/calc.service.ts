ab2eb5af811df99f98d512dfec03ccba209c63da
Mohammad Jawad Barati <mohammad.jawad.barati@energy-robotics.com>
2025-02-13 11:31:20 +0100

    chore: add logs

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -6,23 +6,28 @@ import { Dummy } from './dummy';
 @Injectable()
 export class CalcService {
   sum(request: Observable<SumReq>, subject: ReplaySubject<SumRes>) {
+    console.log('Initialize counter...');
     const counter = new Dummy();
 
     request
       .pipe(
         map((data) => {
+          console.log('Initialize counter...');
           counter.increment();
           return data.number + counter.getCount();
         }),
       )
       .subscribe({
         next: (incrementedNumber) => {
+          console.log('Next handler ' + incrementedNumber);
           subject.next({ result: incrementedNumber });
         },
         error: (err) => {
+          console.log('Error handler ' + err);
           subject.error(err);
         },
         complete: () => {
+          console.log('Complete handler...');
           console.log(counter.getCount());
           subject.complete();
         },
