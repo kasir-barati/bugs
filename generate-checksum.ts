6c035c9deccb0801a8e492a709bfdf0ccf6a8186
Kasir Barati <kasir.barati@gmail.com>
2025-02-14 19:58:25 +0100

    chore: add encoding to generate checksum

--- generate-checksum.ts
+++ generate-checksum.ts
@@ -16,10 +16,10 @@ export function generateChecksum(
       return createHash(algorithm).update(content, "utf8").digest("base64");
     case "CRC32":
       const crc32Checksum = checksums.crc32(content);
-      return Buffer.from(crc32Checksum.toString()).toString("base64");
+      return Buffer.from(crc32Checksum.toString(), "utf8").toString("base64");
     case "CRC32C":
       const crc32cChecksum = checksums.crc32c(content);
-      return Buffer.from(crc32cChecksum.toString()).toString("base64");
+      return Buffer.from(crc32cChecksum.toString(), "utf8").toString("base64");
     case "CRC64NVME":
       const crc64nvmeChecksum = checksums.crc64nvme(content);
       return Buffer.from(crc64nvmeChecksum.buffer).toString("base64");
