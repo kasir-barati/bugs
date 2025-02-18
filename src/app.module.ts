3ffa806dad3e159c2dda534cf7dfd0a2e7dbfd02
Mohammad Jawad Barati <mohammad.jawad.barati@energy-robotics.com>
2025-02-18 15:36:10 +0100

    chore: remove FileUpload import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { UserModule } from './user/user.module';
 import { CalcModule } from './calc/calc.module';
-// import { FileUploaderModule } from './file-uploader/file-uploader.module';
 
 @Module({
   imports: [
