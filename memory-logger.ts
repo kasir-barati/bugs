75f7db73bf6fb76f77c131375081cef6dbdc108e
Mohammad Jawad Barati <kasir.barati@gmail.com>
2025-07-03 19:32:28 +0200

    chore: improve logging

--- memory-logger.ts
+++ memory-logger.ts
@@ -1,12 +1,17 @@
 import { freemem, totalmem } from "os";
 
 export function memoryLogger() {
-  console.log("Free system memory: " + bytesToMb(freemem()) + " Mb");
+  console.log("-".repeat(50));
+  console.log("| Free system memory\t\t:\t" + bytesToMb(freemem()) + " Mb |");
+  console.log("-".repeat(50));
   console.log(
-    "Total amount of system memory: " + bytesToMb(totalmem()) + " Mb"
+    "| Total amount of system memory :\t" + bytesToMb(totalmem()) + " Mb |"
   );
+  console.log("-".repeat(50));
 }
 
+memoryLogger();
+
 function bytesToMb(bytes: number) {
-  return bytes / (1024 * 1024);
+  return Math.ceil(bytes / (1024 * 1024));
 }
